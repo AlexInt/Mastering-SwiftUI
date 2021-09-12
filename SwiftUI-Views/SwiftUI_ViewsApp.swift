3f0450e576215299a1792d982894eb5bd6cddb03
Jimmy <veniveci@aliyun.com>
2021-09-12 11:24:20 +0800

    Initial Commit

---
+++ SwiftUI-Views/SwiftUI_ViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ViewsApp.swift
+//  SwiftUI-Views
+//
+//  Created by jimmy on 2021/9/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
